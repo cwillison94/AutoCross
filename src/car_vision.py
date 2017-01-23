ec6a24477c7c75ca65a6219b9c5ff45fa1b61f2b
cwillison94 <cwillison94@gmail.com>
2017-01-22 22:18:05 -0500

    This file got messed up somehow...

--- src/car_vision.py
+++ src/car_vision.py
@@ -1,92 +1,108 @@
-import RPi.GPIO as GPIO
+#opens up a webcam feed so you can then test your classifer in real time
+#using detectMultiScale
+import numpy as np
+import cv2
 import time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
+CONSECUTIVE_BUFFER_SIZE = 15
+CONSECUTIVE_THRESHOLD = 0.50
 
-#global pin locations
-TRIG = 7
-ECHO = 12
+def drawBoxes(rects, img):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
 
-def setup():
+def drawText(img, message):
+   cv2.putText(img, message, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 1)
 
-    GPIO.setmode(GPIO.BOARD)
-    
-    GPIO.setup(TRIG, GPIO.OUT)
-    GPIO.output(TRIG, 0)
-    GPIO.setup(ECHO, GPIO.IN)
+def detectLines(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    (thresh, img_bw) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
+    img_bw = cv2.GaussianBlur(img_bw, (3, 3), 0)
+    #gray = cv2.equalizeHist(gray)
 
-    #wait for initialization
-    time.sleep(0.1)
+    # smooth and canny edge detection
+    canny = cv2.Canny(img_bw, 50, 200)
+    #canny = cv2.Canny(gray, 50, 200)
 
-def getUltraSonicDistanceCM():
-    GPIO.output(TRIG, 1)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, 0)
+    #ret, thresh = cv2.threshold(equ, 250, 255, cv2.THRESH_BINARY)
 
-    start = time.time()
-    while GPIO.input(ECHO) == 0:
-        pass
-    
-    while GPIO.input(ECHO) == 1:
-        pass
+    lines = cv2.HoughLinesP(canny, 1, np.pi/180, 100, minLineLength = 10, maxLineGap = 10)
 
-    stop = time.time()
+    if (lines is None or len(lines) == 0):
+        return [], img
 
-    return (stop - start) * 17150
+    return lines, img
+
+def detectStopSign(cascade, img):
+    rects = cascade.detectMultiScale(img, 1.3, 4, cv2.CASCADE_SCALE_IMAGE , (20, 20))
+
+    if len(rects) == 0:
+        return [], img
+    rects[:, 2:] += rects[:, :2]
+
+    return rects, img
+
+def detectLanes(lines):
+    print lines[:,0]
+    return lines
+
+def drawLanes(lines, img):
+    for x1,y1,x2,y2 in lines[:,0]:
+        cv2.line(img,(x1, y1),(x2, y2), (0, 255, 0), 2) #rem out if u want to used  polykines
+        #pts = np.array([[x1, y1 ], [x2 , y2 ] ], np.int32)
+        #cv2.polylines(img, [pts], True, (0, 255, 255))
 
 def main():
-    setup()
-
-    while True:
-        dist = getUltraSonicDistanceCM()
-
-        print "Distance: " + str(dist) + " cm"
-        
-        time.sleep(0.5)
-
-if __name__ == "__main__": main()
-import RPi.GPIO as GPIO
-import time
-
-
-#global pin locations
-TRIG = 7
-ECHO = 12
-
-def setup():
-
-    GPIO.setmode(GPIO.BOARD)
+    #cap = cv2.VideoCapture(0)
     
-    GPIO.setup(TRIG, GPIO.OUT)
-    GPIO.output(TRIG, 0)
-    GPIO.setup(ECHO, GPIO.IN)
-
-    #wait for initialization
-    time.sleep(0.1)
-
-def getUltraSonicDistanceCM():
-    GPIO.output(TRIG, 1)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, 0)
-
-    start = time.time()
-    while GPIO.input(ECHO) == 0:
-        pass
+    camera = PiCamera()
+    camera.resolution = (400,400)
+    camera.framerate = 32
+    rawCapture = PiRGBArray(camera, size=(400,400))
     
-    while GPIO.input(ECHO) == 1:
-        pass
+    time.sleep(0.5)
 
-    stop = time.time()
+    cascade = cv2.CascadeClassifier("frontal_stop_sign_cascade.xml")
 
-    return (stop - start) * 17150
+    check_buffer_count = []
 
-def main():
-    setup()
-
-    while True:
-        dist = getUltraSonicDistanceCM()
-
-        print "Distance: " + str(dist) + " cm"
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        #ret, img = cap.read()
         
-        time.sleep(0.5)
+        #camera.capture(rawCapture, format="bgr")
+        img = frame.array
+        rawCapture.truncate(0)
 
-if __name__ == "__main__": main()
+        rects, img = detectStopSign(cascade, img)
+        drawBoxes(rects, img)
+
+        lines, img = detectLines(img)
+
+        #TODO: process lines to detect lanes via length, and location
+        if len(lines) != 0:
+            lines = detectLanes(lines)
+            drawLanes(lines, img)
+
+        check_buffer_count += [len(rects)]
+
+        if (len(check_buffer_count) >= CONSECUTIVE_BUFFER_SIZE):
+            check_buffer_count = check_buffer_count[1:]
+            #check percentage
+            threshold = float(sum(check_buffer_count))/float(len(check_buffer_count))
+            print "Running Threshold: " + str(threshold)
+            if (threshold >= CONSECUTIVE_THRESHOLD):
+                #sign detected
+                drawText(img, "Status: SEND STOP SIGNAL")
+            else:
+                drawText(img, "Status: ")
+
+
+        drawText(img, "Status: ")
+        cv2.imshow("AutoCross Car Control", img)
+
+        if(cv2.waitKey(1) & 0xFF == ord('q')):
+            break
+
+if __name__ == __main__: main()
